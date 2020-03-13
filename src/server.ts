4900edb3284b4c959883903b092b7318dc0dbbf5
kmhoran <kevin.michael.horan@gmail.com>
2020-03-13 06:52:57 -0700

    clean up server -- add web endpoint

--- src/server.ts
+++ src/server.ts
@@ -4,28 +4,25 @@ import { createServer } from "http";
 import compression from "compression";
 import cors from "cors";
 import schema from "./graphQL/schema";
-import {existsSync} from 'fs';
+import { existsSync } from "fs";
 
 import express from "express";
 import path from "path";
-
 const urls = {
-  web: '/web',
-  graphQL : "/graphql"
-}
+  web: "/web",
+  graphQL: "/graphql"
+};
 
 const app = express();
 const port = process.env.PORT || 3001;
 const server = new ApolloServer({
-    schema,
-    validationRules: [depthLimit(7)]
-  });
+  schema,
+  validationRules: [depthLimit(7)]
+});
 
-app.use("*", cors());
+app.use("*", cors(), getHost);
 app.use(compression());
 server.applyMiddleware({ app, path: urls.graphQL });
-
-
 
 serveStaticHtml(app);
 
@@ -34,17 +31,24 @@ const httpServer = createServer(app);
 httpServer.listen({ port }, onServerUpSuccess);
 
 async function onServerUpSuccess() {
-  console.log(`\nGraphQL is now running on http://localhost:${port}${urls.graphQL}`);
+  console.log(
+    `\nGraphQL is now running on http://localhost:${port}${urls.graphQL}`
+  );
   console.log(`\nWeb is now running on http://localhost:${port}${urls.web}`);
 }
 
+function serveStaticHtml(app: any) {
+  const resourcePath = path.join(__dirname, "ui");
+  if (existsSync(resourcePath))
+    app.use(urls.web, getHost, express.static(resourcePath));
 
-function serveStaticHtml(app:any){
-  const resourcePath = path.join(__dirname, 'ui');
-  if(existsSync(resourcePath)) app.use(urls.web, express.static(resourcePath));
-  
   // TODO come up with a better fallback
-  var html = "<!DOCTYPE html>\n<html>\n    <head>\n    </head>\n <body>\n      <h1>Site Down</h1>\n   </body>\n</html>"
-  app.use(urls.web, (req:any, res:any) => res.send(html));
+  var html =
+    "<!DOCTYPE html>\n<html>\n    <head>\n    </head>\n <body>\n      <h1>Site Down</h1>\n   </body>\n</html>";
+  app.use(urls.web, (req: any, res: any) => res.send(html));
+}
 
+function getHost(req: any, res: any, next: any) {
+  console.log("host:", req.headers.host);
+  next();
 }
