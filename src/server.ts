e3e59319248b67d4414dedb7a0c23f8ada7cdf93
kmhoran <kevin.michael.horan@gmail.com>
2020-03-14 07:47:54 -0700

    stop servin static files. focus on graphQL

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,6 @@ import { existsSync } from "fs";
 import express from "express";
 import path from "path";
 const urls = {
-  web: "/web",
   graphQL: "/graphql"
 };
 
@@ -24,8 +23,6 @@ app.use("*", cors(), getHost);
 app.use(compression());
 server.applyMiddleware({ app, path: urls.graphQL });
 
-serveStaticHtml(app);
-
 const httpServer = createServer(app);
 
 httpServer.listen({ port }, onServerUpSuccess);
@@ -34,19 +31,8 @@ async function onServerUpSuccess() {
   console.log(
     `\nGraphQL is now running on http://localhost:${port}${urls.graphQL}`
   );
-  console.log(`\nWeb is now running on http://localhost:${port}${urls.web}`);
 }
 
-function serveStaticHtml(app: any) {
-  const resourcePath = path.join(__dirname, "ui");
-  if (existsSync(resourcePath))
-    app.use(urls.web, getHost, express.static(resourcePath));
-
-  // TODO come up with a better fallback
-  var html =
-    "<!DOCTYPE html>\n<html>\n    <head>\n    </head>\n <body>\n      <h1>Site Down</h1>\n   </body>\n</html>";
-  app.use(urls.web, (req: any, res: any) => res.send(html));
-}
 
 function getHost(req: any, res: any, next: any) {
   console.log("host:", req.headers.host);
